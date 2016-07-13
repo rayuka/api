e79632eb12374f58a5db3f2cf9c1506d6e31938a
Mukesh Chandra <Mukesh Chandra>
2016-07-14 01:10:22 +0530

    update root and pass

--- show.php
+++ show.php
@@ -1,5 +1,7 @@
 <?php
 $con=mysqli_connect("localhost","sati","sati","project_db");
+if($con)
+	echo "rathore";
 if (mysqli_connect_errno($con))
 {
    echo '{"query_result":"ERROR"}';
@@ -7,6 +9,7 @@ if (mysqli_connect_errno($con))
 $sql="SELECT * FROM `user`;";
 $result = mysqli_query($con,$sql);
 while($row = mysqli_fetch_array($result)){
+	echo "fuck";
     array_push($r,array(
         'id'=>$row['id'],
         'name'=>$row['name'],
