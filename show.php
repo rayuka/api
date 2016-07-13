127a405d9210903204a569c4aed72a405d262957
Mukesh Chandra <Mukesh Chandra>
2016-07-14 00:31:39 +0530

    update root and pass

--- show.php
+++ show.php
@@ -1,4 +1,17 @@
 <?php
-for($i=0;$i<10;$i++)
-echo "yeah here i am \n";
+$con=mysqli_connect("localhost","sati","sati","project_db");
+if (mysqli_connect_errno($con))
+{
+   echo '{"query_result":"ERROR"}';
+}
+$sql="SELECT * FROM `user`;";
+$result = mysqli_query($con,$sql);
+while($row = mysqli_fetch_array($result)){
+    array_push($r,array(
+        'id'=>$row['id'],
+        'name'=>$row['name'],
+        'location'=>$row['location']
+    ));
+}
+echo json_encode(array("r"=>$r));
 ?>
